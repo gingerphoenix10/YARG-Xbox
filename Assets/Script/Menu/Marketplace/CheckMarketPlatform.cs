fab329fa6ec343357b60d205871bb51f97d8b0e4
Phoenix <phoenix@gingerphoenix10.com>
2025-11-27 02:35:43 +0000

    Update to some newer version of nightly idk

---
+++ Assets/Script/Menu/Marketplace/CheckMarketPlatform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace YARG
+{
+    public class CheckMarketPlatform : MonoBehaviour
+    {
+        private void Awake()
+        {
+#if UNITY_STANDALONE
+            Destroy(gameObject);
+#endif
+        }
+    }
+}

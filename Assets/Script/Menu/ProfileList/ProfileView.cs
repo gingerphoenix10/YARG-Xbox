3b0cc3d808107d4930747f3f820ec094dffe33bc
Phoenix Payne-Barnes <Phoenix@gingerphoenix10.com>
2026-01-03 04:52:18 +0000

    Hopefully fix iOS vocals

--- Assets/Script/Menu/ProfileList/ProfileView.cs
+++ Assets/Script/Menu/ProfileList/ProfileView.cs
@@ -238,6 +238,8 @@ namespace YARG.Menu.ProfileList
 
         public async UniTask<bool> PromptAddDevice()
         {
+            if (!Application.HasUserAuthorization(UserAuthorization.Microphone))
+                await Application.RequestUserAuthorization(UserAuthorization.Microphone).ToUniTask();
             var dialog = DialogManager.Instance.ShowList("Add Device\n" +
                 "<alpha=#44><size=65%><line-height=50%>\nIf your device does not show up, try hitting a button/pad on " +
                 "it first, and then retry.</size>");
